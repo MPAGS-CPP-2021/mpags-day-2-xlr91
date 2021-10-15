cd6dd4f22ced9fb2f654da4a470897fae92c7a01
Emir <emirmuhammad08@yahoo.com>
2021-10-15 11:56:07 +0000

    Move and declare processCL and transformChar

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -1,7 +1,108 @@
+// standard library includes
 #include <cctype>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+
+
+//define functions
+
+bool processCommandLine(
+    const std::vector<std::string>& args,
+    bool& helpRequested,
+    bool& versionRequested,
+    std::string& inputFileName,
+    std::string& outputFileName );
+
+std::string transformChar( const char in_char );
+
+
+int main(int argc, char* argv[])
+{
+    // Convert the command-line arguments into a more easily usable form
+    const std::vector<std::string> cmdLineArgs{argv, argv + argc};
+    
+
+    // Options that might be set by the command-line arguments
+    bool helpRequested{false};
+    bool versionRequested{false};
+    std::string inputFile{""};
+    std::string outputFile{""};
+
+    bool processCLOutput{false};
+
+
+    processCLOutput = processCommandLine(cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile);
+
+    
+    if(processCLOutput){
+        return 1; //indicates that there is an error
+    }
+    
+
+
+    // Handle help, if requested
+    if (helpRequested) {
+        // Line splitting for readability
+        std::cout
+            << "Usage: mpags-cipher [-h/--help] [--version] [-i <file>] [-o <file>]\n\n"
+            << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
+            << "Available options:\n\n"
+            << "  -h|--help        Print this help message and exit\n\n"
+            << "  --version        Print version information\n\n"
+            << "  -i FILE          Read text to be processed from FILE\n"
+            << "                   Stdin will be used if not supplied\n\n"
+            << "  -o FILE          Write processed text to FILE\n"
+            << "                   Stdout will be used if not supplied\n\n"
+            << std::endl;
+        // Help requires no further action, so return from main
+        // with 0 used to indicate success
+        return 0;
+    }
+
+    // Handle version, if requested
+    // Like help, requires no further action,
+    // so return from main with zero to indicate success
+    if (versionRequested) {
+        std::cout << "0.1.0" << std::endl;
+        return 0;
+    }
+
+
+
+    // Initialise variables
+    char inputChar{'x'};
+    std::string inputText;
+
+    // Read in user input from stdin/file
+    // Warn that input file option not yet implemented
+    if (!inputFile.empty()) {
+        std::cerr << "[warning] input from file ('" << inputFile
+                  << "') not implemented yet, using stdin\n";
+    }
+
+    // loop over each character from user input
+    while (std::cin >> inputChar) {
+        inputText += transformChar(inputChar);
+        // If the character isn't alphabetic or numeric, DONT add it
+    }
+
+    // Print out the transliterated text
+
+    // Warn that output file option not yet implemented
+    if (!outputFile.empty()) {
+        std::cerr << "[warning] output to file ('" << outputFile
+                  << "') not implemented yet, using stdout\n";
+    }
+
+    std::cout << inputText << std::endl;
+
+    // No requirement to return from main, but we do so for clarity
+    // and for consistency with other functions
+    return 0;
+}
+
 
 
 bool processCommandLine(
@@ -99,90 +200,4 @@ std::string transformChar( const char in_char ){
             break;
     }
     return outputText;
-}
-
-
-int main(int argc, char* argv[])
-{
-    // Convert the command-line arguments into a more easily usable form
-    const std::vector<std::string> cmdLineArgs{argv, argv + argc};
-    
-
-    // Options that might be set by the command-line arguments
-    bool helpRequested{false};
-    bool versionRequested{false};
-    std::string inputFile{""};
-    std::string outputFile{""};
-
-    bool processCLOutput{false};
-
-
-    processCLOutput = processCommandLine(cmdLineArgs, helpRequested, versionRequested, inputFile, outputFile);
-
-    
-    if(processCLOutput){
-        return 1; //indicates that there is an error
-    }
-    
-
-
-    // Handle help, if requested
-    if (helpRequested) {
-        // Line splitting for readability
-        std::cout
-            << "Usage: mpags-cipher [-h/--help] [--version] [-i <file>] [-o <file>]\n\n"
-            << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
-            << "Available options:\n\n"
-            << "  -h|--help        Print this help message and exit\n\n"
-            << "  --version        Print version information\n\n"
-            << "  -i FILE          Read text to be processed from FILE\n"
-            << "                   Stdin will be used if not supplied\n\n"
-            << "  -o FILE          Write processed text to FILE\n"
-            << "                   Stdout will be used if not supplied\n\n"
-            << std::endl;
-        // Help requires no further action, so return from main
-        // with 0 used to indicate success
-        return 0;
-    }
-
-    // Handle version, if requested
-    // Like help, requires no further action,
-    // so return from main with zero to indicate success
-    if (versionRequested) {
-        std::cout << "0.1.0" << std::endl;
-        return 0;
-    }
-
-
-
-    // Initialise variables
-    char inputChar{'x'};
-    std::string inputText;
-
-    // Read in user input from stdin/file
-    // Warn that input file option not yet implemented
-    if (!inputFile.empty()) {
-        std::cerr << "[warning] input from file ('" << inputFile
-                  << "') not implemented yet, using stdin\n";
-    }
-
-    // loop over each character from user input
-    while (std::cin >> inputChar) {
-        inputText += transformChar(inputChar);
-        // If the character isn't alphabetic or numeric, DONT add it
-    }
-
-    // Print out the transliterated text
-
-    // Warn that output file option not yet implemented
-    if (!outputFile.empty()) {
-        std::cerr << "[warning] output to file ('" << outputFile
-                  << "') not implemented yet, using stdout\n";
-    }
-
-    std::cout << inputText << std::endl;
-
-    // No requirement to return from main, but we do so for clarity
-    // and for consistency with other functions
-    return 0;
 }
