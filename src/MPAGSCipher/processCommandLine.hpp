99c41a89dfe644208a5c31b153720618c45fedfe
Emir <emirmuhammad08@yahoo.com>
2021-10-15 12:34:53 +0000

    Modularise TransformChar and processCommandLine

---
+++ src/MPAGSCipher/processCommandLine.hpp
@@ -0,0 +1,16 @@
+#ifndef MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+#define MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+bool processCommandLine(
+    const std::vector<std::string>& args,
+    bool& helpRequested,
+    bool& versionRequested,
+    std::string& inputFileName,
+    std::string& outputFileName );
+
+#endif
--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -4,20 +4,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+ 
+#include "TransformChar.hpp"
+#include "processCommandLine.hpp"
 
 
 //define functions
-
-bool processCommandLine(
-    const std::vector<std::string>& args,
-    bool& helpRequested,
-    bool& versionRequested,
-    std::string& inputFileName,
-    std::string& outputFileName );
-
-std::string transformChar( const char in_char );
-
-
 int main(int argc, char* argv[])
 {
     // Convert the command-line arguments into a more easily usable form
@@ -104,100 +96,3 @@ int main(int argc, char* argv[])
 }
 
 
-
-bool processCommandLine(
-    const std::vector<std::string>& args,
-    bool& helpRequested,
-    bool& versionRequested,
-    std::string& inputFileName,
-    std::string& outputFileName ){
-
-    // Process command line arguments - ignore zeroth element, as we know this
-    // to be the program name and don't need to worry about it
-    const std::size_t nCmdLineArgs{args.size()};
-    for (std::size_t i{1}; i < nCmdLineArgs; ++i) {
-        if (args[i] == "-h" || args[i] == "--help") {
-            helpRequested = true;
-        } else if (args[i] == "--version") {
-            versionRequested = true;
-        } else if (args[i] == "-i") {
-            // Handle input file option
-            // Next element is filename unless "-i" is the last argument
-            if (i == nCmdLineArgs - 1) {
-                std::cerr << "[error] -i requires a filename argument"
-                          << std::endl;
-                // exit main with non-zero return to indicate failure
-                return 1;
-            } else {
-                // Got filename, so assign value and advance past it
-                inputFileName = args[i + 1];
-                ++i;
-            }
-        } else if (args[i] == "-o") {
-            // Handle output file option
-            // Next element is filename unless "-o" is the last argument
-            if (i == nCmdLineArgs - 1) {
-                std::cerr << "[error] -o requires a filename argument"
-                          << std::endl;
-                // exit main with non-zero return to indicate failure
-                return 1;
-            } else {
-                // Got filename, so assign value and advance past it
-                outputFileName = args[i + 1];
-                ++i;
-            }
-        } else {
-            // Have an unknown flag to output error message and return non-zero
-            // exit status to indicate failure
-            std::cerr << "[error] unknown argument '" << args[i]
-                      << "'\n";
-            return 1;
-        }
-    }
-    return 0;
-}
-
-
-std::string transformChar( const char in_char ){
-// Uppercase alphabetic characters
-    std::string outputText;
-    if (std::isalpha(in_char)) {
-        outputText += std::toupper(in_char);
-        return outputText;
-    }
-
-    // Transliterate digits to English words
-    switch (in_char) {
-        case '0':
-            outputText += "ZERO";
-            break;
-        case '1':
-            outputText += "ONE";
-            break;
-        case '2':
-            outputText += "TWO";
-            break;
-        case '3':
-            outputText += "THREE";
-            break;
-        case '4':
-            outputText += "FOUR";
-            break;
-        case '5':
-            outputText += "FIVE";
-            break;
-        case '6':
-            outputText += "SIX";
-            break;
-        case '7':
-            outputText += "SEVEN";
-            break;
-        case '8':
-            outputText += "EIGHT";
-            break;
-        case '9':
-            outputText += "NINE";
-            break;
-    }
-    return outputText;
-}
